1000d0d00547026dafe0708f6bf519b494fd178a
MadDogFG <1347797750@qq.com>
2025-09-03 00:15:57 +0800

    实现消息发布函数

--- src/services/IdentityService/AperturePlus.IdentityService.Infrastructure/MessageBus/RabbitMQMessageBus.cs
+++ src/services/IdentityService/AperturePlus.IdentityService.Infrastructure/MessageBus/RabbitMQMessageBus.cs
@@ -19,9 +19,22 @@ namespace AperturePlus.IdentityService.Infrastructure.MessageBus
             this.channel = channel;
         }
 
-        public void Publish(string exchange, string routingKey, object data)
+        public async Task Publish(string exchange, string routingKey, object data)
         {
-            throw new NotImplementedException();
+            if (data == null)
+            {
+                return;
+            }
+            var body = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(data));
+            var properties = new BasicProperties();//消息的其他属性,比如消息头什么的
+            try
+            {
+                await channel.BasicPublishAsync(exchange, routingKey, false, properties, body);//mandatory设为false表示如果没有队列与该routingKey匹配，消息将被丢弃，设置为true则会返回给生产者
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
