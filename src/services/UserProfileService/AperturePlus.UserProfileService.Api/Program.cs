296824f6cc9de9c158095fe04ade5e9125eb7752
MadDogFG <1347797750@qq.com>
2025-09-08 19:31:31 +0800

    创建UserProfile根实体

---
+++ src/services/UserProfileService/AperturePlus.UserProfileService.Api/Program.cs
@@ -0,0 +1,107 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
+
+namespace AperturePlus.UserProfileService.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //builder.Services.AddDbContext<ActivityServiceDbContext>(opt =>
+            //{
+            //    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection"));
+            //});
+
+            builder.Services.AddAuthentication()
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // 验证Issuer是否合法
+                        ValidateIssuer = true,
+                        // 验证Audience是否合法
+                        ValidateAudience = true,
+                        // 验证令牌的生命周期
+                        ValidateLifetime = true,
+                        // 验证签名密钥是否有效
+                        ValidateIssuerSigningKey = true,
+                        // 设置合法的Issuer
+                        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                        // 设置合法的Audience
+                        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                        // 签名密钥）
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+                    };
+                });
+            builder.Services.AddMediatR(cfg =>
+            {
+                //cfg.RegisterServicesFromAssembly(typeof(CreateActivityCommandHandler).Assembly);
+            });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                var scheme = new OpenApiSecurityScheme()
+                {
+                    Description = "Authorization 请求头. \r\n示例: 'Bearer XXXXXX'",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Authorization"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                };
+                c.AddSecurityDefinition("Authorization", scheme);
+                var requirement = new OpenApiSecurityRequirement();
+                requirement[scheme] = new List<string>();
+                c.AddSecurityRequirement(requirement);
+            });
+
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQConnection"))
+                };
+                Console.WriteLine(builder.Configuration.GetConnectionString("RabbitMQConnection"));
+                var connection = await factory.CreateConnectionAsync();
+                builder.Services.AddSingleton<IConnection>(connection);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("无法连接到RabbitMQ服务器: " + ex.Message);
+            }
+
+            //builder.Services.AddHostedService<UserEventsConsumer>();//注册后台消费者服务
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ src/services/UserProfileService/AperturePlus.UserProfileService.Domain/Entities/UserProfile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AperturePlus.UserProfileService.Domain.Entities
+{
+    public class UserProfile
+    {
+        public Guid UserId { get; private set; }
+        public string UserName { get; private set; }
+        public string Bio { get; private set; }//个人简介
+        public string AvatarUrl { get; private set; }//头像URL
+
+        public UserProfile() { }
+
+        private UserProfile(Guid userId, string userName, string bio, string avatarUrl)
+        {
+            UserId = userId;
+            UserName = userName;
+            Bio = bio;
+            AvatarUrl = avatarUrl;
+        }
+
+        public static UserProfile CreateUserProfile(Guid userId, string userName, string bio, string avatarUrl)
+        {
+            return new UserProfile(userId, userName, bio,avatarUrl);
+        }
+
+        public void UpdateUserProfile(string? bio, string? avatarUrl)
+        {
+            if (bio != null)
+            {
+                Bio = bio;
+            }
+            if (avatarUrl != null)
+            {
+                AvatarUrl = avatarUrl;
+            }
+        }
+    }
+}
