3280b0cc8319f6bb8677cb773fb38f3a1112c155
MadDogFG <1347797750@qq.com>
2025-09-08 17:56:27 +0800

    将更新活动的UpdateActivityRequest属性改为可空

--- src/services/ActivityService/Api/DTOs/UpdateActivityRequest.cs
+++ src/services/ActivityService/Api/DTOs/UpdateActivityRequest.cs
@@ -4,11 +4,11 @@ namespace AperturePlus.ActivityService.Api.DTOs
 {
     public record class UpdateActivityRequest
     {
-        public string ActivityTitle { get; set; }
-        public string ActivityDescription { get; set; }
-        public Location ActivityLocation { get; set; }
-        public DateTime ActivityStartTime { get; set; }
-        public Decimal Fee { get; set; } //活动费用
-        public List<RoleRequirement> RoleRequirements { get; set; } = new List<RoleRequirement>(); //角色需求列表
+        public string? ActivityTitle { get; set; }
+        public string? ActivityDescription { get; set; }
+        public Location? ActivityLocation { get; set; }
+        public DateTime? ActivityStartTime { get; set; }
+        public Decimal? Fee { get; set; }//活动费用
+        public List<RoleRequirement>? RoleRequirements { get; set; }//角色需求列表
     }
 }
