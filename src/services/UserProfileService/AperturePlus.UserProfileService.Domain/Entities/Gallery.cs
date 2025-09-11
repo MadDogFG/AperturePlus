5ebee67890b0079083b236512c8adfd8c13a87c9
MadDogFG <1347797750@qq.com>
2025-09-11 23:25:43 +0800

    为作品集功能设计相关实体

---
+++ src/services/UserProfileService/AperturePlus.UserProfileService.Domain/Entities/Gallery.cs
@@ -0,0 +1,78 @@
+﻿using AperturePlus.UserProfileService.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AperturePlus.UserProfileService.Domain.Entities
+{
+    public class Gallery
+    {
+        public Guid GalleryId { get; private set; }
+        public string GalleryName { get; private set; }
+        public string? CoverPhotoUrl { get; private set; }//封面图
+        private readonly List<Photo> _photos = new();
+        public IReadOnlyCollection<Photo> Photos => _photos.AsReadOnly();//只读集合，防止外部能直接通过属性添加或者删除图片
+        public DateTime CreatedAt { get; private set; }
+
+        private Gallery(string galleryName)
+        {
+            GalleryId = Guid.NewGuid();
+            GalleryName = galleryName;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public static Gallery CreateGallery(string galleryName)
+        {
+            if (string.IsNullOrEmpty(galleryName))
+            {
+                throw new ArgumentException("相册名称不能为空");
+            }
+            return new Gallery(galleryName);
+        }
+
+        public void AddPhoto(Photo photo)
+        {
+            if (photo == null)
+            {
+                throw new ArgumentNullException("图片不能为空");
+            }
+            _photos.Add(photo);
+            //如果是第一张图，设置为封面图
+            if (_photos.Count == 1)
+            {
+                CoverPhotoUrl = photo.PhotoUrl;
+            }
+        }
+
+        public void RemovePhoto(Guid photoId)
+        {
+            var photo = _photos.FirstOrDefault(p=>p.PhotoId == photoId);
+            if (photo == null) 
+            {
+                throw new ArgumentException("图片不存在");
+            }
+            _photos.Remove(photo);
+        }
+
+        public void SetCoverPhoto(Guid photoId)
+        {
+            var photoUrl = _photos.Where(p=>p.PhotoId==photoId).Select(p => p.PhotoUrl).FirstOrDefault();
+            if (string.IsNullOrEmpty(photoUrl))
+            {
+                throw new ArgumentException("图片不存在");
+            }
+            CoverPhotoUrl = photoUrl;
+        }
+
+        public void RenameGallery(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                throw new ArgumentException("相册名称不能为空");
+            }
+            GalleryName = newName;
+        }
+    }
+}
---
+++ src/services/UserProfileService/AperturePlus.UserProfileService.Domain/ValueObjects/Photo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AperturePlus.UserProfileService.Domain.ValueObjects
+{
+    public record class Photo
+    {
+        public Guid PhotoId { get; init; }
+        public string PhotoUrl { get; init; }
+        public List<string> Tags { get; private set; } = new List<string>();//留着未来调用模型进行智能分类
+        public DateTime UploadedAt { get; init; }
+
+        private Photo(string photoUrl)
+        {
+            PhotoId = Guid.NewGuid();
+            PhotoUrl = photoUrl;
+            UploadedAt = DateTime.UtcNow;
+        }
+
+        public static Photo CreatePhoto(string photoUrl)
+        {
+            if (string.IsNullOrEmpty(photoUrl))
+            {
+                throw new ArgumentException("图片路径为空");
+            }
+            return new Photo(photoUrl);
+        }
+    }
+}
