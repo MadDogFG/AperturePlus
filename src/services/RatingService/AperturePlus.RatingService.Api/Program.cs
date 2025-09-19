e4e9f1d2360e349ddaf59b95c9ddf8b6265a06b6
MadDogFG <1347797750@qq.com>
2025-09-19 23:49:36 +0800

    修改Rating微服务的配置

--- src/services/RatingService/AperturePlus.RatingService.Api/Program.cs
+++ src/services/RatingService/AperturePlus.RatingService.Api/Program.cs
@@ -1,9 +1,18 @@
 
+using AperturePlus.RatingService.Application.Handlers;
+using AperturePlus.RatingService.Application.Interfaces;
+using AperturePlus.RatingService.Infrastructure.Persistence;
+using AperturePlus.RatingService.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
+
 namespace AperturePlus.RatingService.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -11,18 +20,90 @@ namespace AperturePlus.RatingService.Api
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<RatingServiceDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection"));
+            });
+
+            builder.Services.AddAuthentication()
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // 验证Issuer是否合法
+                        ValidateIssuer = true,
+                        // 验证Audience是否合法
+                        ValidateAudience = true,
+                        // 验证令牌的生命周期
+                        ValidateLifetime = true,
+                        // 验证签名密钥是否有效
+                        ValidateIssuerSigningKey = true,
+                        // 设置合法的Issuer
+                        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                        // 设置合法的Audience
+                        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                        // 签名密钥）
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+                    };
+                });
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(SubmitRatingCommandHandler).Assembly);
+            });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                var scheme = new OpenApiSecurityScheme()
+                {
+                    Description = "Authorization 请求头. \r\n示例: 'Bearer XXXXXX'",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Authorization"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                };
+                c.AddSecurityDefinition("Authorization", scheme);
+                var requirement = new OpenApiSecurityRequirement();
+                requirement[scheme] = new List<string>();
+                c.AddSecurityRequirement(requirement);
+            });
+
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQConnection"))
+                };
+                Console.WriteLine(builder.Configuration.GetConnectionString("RabbitMQConnection"));
+                var connection = await factory.CreateConnectionAsync();
+                builder.Services.AddSingleton<IConnection>(connection);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("无法连接到RabbitMQ服务器: " + ex.Message);
+            }
+
+            //builder.Services.AddHostedService<UserEventsConsumer>();//注册后台消费者服务
+            builder.Services.AddScoped<IRatingRepository, RatingRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
@@ -30,5 +111,6 @@ namespace AperturePlus.RatingService.Api
 
             app.Run();
         }
+    }
     }
 }
